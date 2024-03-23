50da115e139743f8e641e153924d3c71e39a324a
adan eshete <adaneeshete560@gmail.com>
2024-03-23 20:14:03 +0300

    commit

--- frontend/component/ControlSystem.js
+++ frontend/component/ControlSystem.js
@@ -1,12 +1,100 @@
-import React from 'react'
-import { View,Text } from 'react-native'
-import{ styles }from '../styles/controlSystem'
-const ControlSystem = () => {
-  return (
-    <View style={styles.main}>
-     <Text style={styles.text1}>Turn System On/Off</Text> 
-    </View>
-  )
+import React from "react";
+import { View, Text, TouchableOpacity, TextInput,ScrollView } from "react-native";
+import { styles } from "../styles/controlSystem";
+import { useState, useEffect } from "react";
+
+//logic to on and off the system
+function onToOff() {
+  console.log("the system is on please click to turn of ");
+}
+function offToOn() {
+  console.log("the sytem is off click to turn on ");
 }
 
-export default ControlSystem
+const ControlSystem = () => {
+  // const navigation = useNavigation();
+  const [turnSystemOn, setTurnSystemOn] = useState(false);
+  const [startTime, setStartTime] = useState("");
+  const [endTime, setEndTime] = useState("");
+  const [frequency, setFrequency] = useState("");
+
+  const handleToggleTurnSystem = () => {
+    setTurnSystemOn(!turnSystemOn);
+  };
+  const handleAdjustWateringSchedule = () => {
+    navigation.navigate("AdjustWateringScheduleScreen"); // replace with the actual name of your screen
+  };
+
+  return (
+    <View style={styles.main}>
+      <ScrollView>
+      <Text style={styles.text1}>Turn System On/Off</Text>
+      <View style={styles.main1}>
+        <TouchableOpacity
+          onPress={() => {
+            offToOn();
+          }}
+        >
+          <Text style={styles.text2}>Turn System Off</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            onToOff();
+          }}
+        >
+          <Text style={styles.text2}>Turn System On</Text>
+        </TouchableOpacity>
+      </View>
+      <Text style={styles.text1}>Adjust Water schedule</Text>
+      <View style={styles.main2}>
+        <TextInput
+          value={startTime}
+          onChangeText={setStartTime}
+          placeholder="Start Time"
+          keyboardType="numeric"
+          style={styles.textInput}
+        />
+        <TextInput
+          value={endTime}
+          onChangeText={setEndTime}
+          placeholder="End Time"
+          keyboardType="numeric"
+          style={styles.textInput}
+        />
+        <TextInput
+          value={frequency}
+          onChangeText={setFrequency}
+          placeholder="Frequency"
+          keyboardType="numeric"
+          style={styles.textInput}
+        />
+      </View>
+      <Text style={styles.text1}>Set Specific Zone</Text>
+      <View style={styles.main2}>
+        <View style={styles.main3}>
+          <Text style={styles.text3}>Zone A</Text>
+          <TextInput  style={styles.input1} placeholder="Watering duration"></TextInput>
+          
+          
+          </View>
+        <View  style={styles.main3}>
+          <Text style={styles.text3}>Zone B</Text>
+          <TextInput  style={styles.input1} placeholder="Watering duration"></TextInput>
+          </View>
+          
+        <View style={styles.main3}>
+          <Text style={styles.text3}>Zone C</Text>
+          <TextInput  style={styles.input1} placeholder="Watering duration"></TextInput>
+          </View>
+          
+      </View>
+      <Text style={styles.text1}>Others</Text>
+      <TouchableOpacity style={styles.touch2}>
+        <Text>Additional settings</Text>
+      </TouchableOpacity>
+      </ScrollView>
+    </View>
+  );
+};
+
+export default ControlSystem;
--- frontend/styles/controlSystem.js
+++ frontend/styles/controlSystem.js
@@ -11,5 +11,63 @@ export const styles = StyleSheet.create({
       marginStart: 12,
       fontWeight: "bold",
     },
+    main1:{
+    margin:20,
+    borderRadius:20,
+      backgroundColor:'white',
+      height:45,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+     
+
+    },
+    text2:{
+      marginHorizontal: 5,
+      flex: 1,
+      margin:12,
+      fontWeight:'bold'
+    },
+    main2:{
+      backgroundColor: '#faf0e6', 
+    borderRadius: 8, 
+    borderWidth: 3, 
+    borderColor: 'white', 
+     padding: 10,
+     margin:20
+    },
+    textInput: {
+      borderWidth: 1,
+      borderColor: '#ccc',
+      borderRadius: 5,
+      padding: 10,
+      marginBottom: 10,
+    },
+    optionText: {
+      fontSize: 16,
+      marginTop: 10,
+    },
+    main3:{
+      flexDirection:'row'
+    },
+    text3:{
+      fontWeight:'bold',
+      marginTop:25,
+      height:50,
+    },
+    input1:{
+  margin:12,
+  backgroundColor:'white',
+  height:50,
+  alignItems:'flex-end',
+  marginStart:100,
+  width:150,
+  borderRadius:15
+
+    },
+    touch2:{
+      borderWidth:3,
+      borderRadius:12,
+    }
+
  
 });
